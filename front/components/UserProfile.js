b9c392adfc8eb238356f21e78e63ec7460bf024e
sarifor <iread1q84@gmail.com>
2024-06-08 11:37:52 +0900

    Update: userProfile.js using antd Card

--- front/components/UserProfile.js
+++ front/components/UserProfile.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { SignatureOutlined, HeartOutlined, TeamOutlined } from '@ant-design/icons';
+import { Card } from 'antd';
+const { Meta } = Card;
 
 const UserProfile = () => {
   return (
     <>
-      This is user profile page!
+      <Card
+        style={{
+          width: 300,
+        }}
+        cover={
+          <img
+            alt="Hello Kitty"
+            src="https://www.sanrio.co.jp/wp-content/uploads/2022/06/mv-hellokitty.png"
+          />
+        }
+        actions={[ // React JSX 배열에선 키 붙여줘야 함
+          <SignatureOutlined key="tweet" />,
+          <HeartOutlined key="followings" />,
+          <TeamOutlined key="followers" />,
+        ]}
+      >
+        <Meta
+          // avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />}
+          title="Card title"
+          description="This is the description"
+        />
+      </Card>
     </>
   )
 }

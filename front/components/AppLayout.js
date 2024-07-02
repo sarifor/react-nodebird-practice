84c32141b0ed5f020c82494b6fff2ca72f760806
sarifor <iread1q84@gmail.com>
2024-07-02 10:11:09 +0900

    Improve: Log latest hashtag value on browser console using useState, useEffect, and Antd Input.Search's onSearch prop

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,7 +1,7 @@
 // # AppLayout.js
 // - 일부 공통인 애들용
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
@@ -13,33 +13,48 @@ import UserProfile from './UserProfile';
 import LoginForm from './LoginForm';
 
 const Search = styled(Input.Search)`
-  vertical-align: middle;
+vertical-align: middle;
 `;
 
-const items = [
-  {
-    label: <Link href="/"><a>Home</a></Link>,
-    key: '/',
-  },
-  {
-    label: <Link href="/profile"><a>Profile</a></Link>,
-    key: 'profile',
-  },
-  {
-    label: <Link href="/signup"><a>Sign Up</a></Link>,
-    key: 'signup',
-  },  
-  {
-    label: <Search placeholder="input hashtag" enterButton="Search" />,
-    key: 'search',
-  },    
-];
 const AppLayout = ({ children }) => {
-  // 서버 대신 더미 데이터 사용
+  // 로그인 여부 상태: 서버 대신 더미 데이터 사용
   const [isLoggedIn, setIsLoggedIn] = useState(false); // Q. setIsLoggedIn은 useCallback으로 감싸지 않아도 되나?
 
+  // hashtag 상태
+  const [hashtag, setHashtag] = useState('');
+
+  // hashtag 상태 변경
+  const handleHashtag = (value) => {
+    setHashtag(value);
+    // console.log(hashtag); // 이 경우, 이전 상태를 출력해버림
+  };
+
+  // hashtag 상태가 변경될 때마다, 최신값을 콘솔에 출력
+  useEffect(() => {
+    console.log(hashtag);
+  }, [hashtag])
+  
   const router = useRouter();
   const current = router.pathname;
+
+  const items = [
+    {
+      label: <Link href="/"><a>Home</a></Link>,
+      key: '/',
+    },
+    {
+      label: <Link href="/profile"><a>Profile</a></Link>,
+      key: 'profile',
+    },
+    {
+      label: <Link href="/signup"><a>Sign Up</a></Link>,
+      key: 'signup',
+    },  
+    {
+      label: <Search placeholder="input hashtag" enterButton="Search" onSearch={handleHashtag} />, // onSearch: Ant Design Input.Search의 property
+      key: 'search',
+    },    
+  ];
 
   return (
     <div>

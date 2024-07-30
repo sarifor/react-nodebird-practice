f43801a1d7ac4376c10f4b48db86c315e67a59b6
Sarifor <iread1q84@gmail.com>
2024-07-30 10:07:26 +0900

    Fix: pass complete post object to PostCard component to display all properties

--- front/pages/index.js
+++ front/pages/index.js
@@ -13,10 +13,13 @@ const Home = () => {
   const { isLoggedIn } = useSelector((state) => state.user);
   const { mainPosts } = useSelector((state) => state.post);
 
+  // 컴포넌트 렌더링
+  // - map 함수로 반복되는 컴포넌트 렌더링
+  // - 배열 내 각 요소를 원하는 규칙에 따라 변환한 후, 그 결과로 새로운 요소를 반환
   return (
     <AppLayout>
       {isLoggedIn && <PostForm />}
-      {mainPosts.map((post) => <PostCard key={post.id} post={post.content} />)}
+      {mainPosts.map((post) => <PostCard key={post.id} post={post} />)}
     </AppLayout>
   )
 }

3c39877f141221467527b2c0efff75db1a6f4be1
Sarifor <iread1q84@gmail.com>
2024-08-14 11:39:42 +0900

    Add: component with SCSS
    Use nesting and ampersand

---
+++ code-practice/react/09-component-styling/src/09-02-Sass/09-02-00-Intro/SailorMoonColors.js
@@ -0,0 +1,15 @@
+import './SailorMoonColors.scss';
+
+const SailorMoonColors = () => {
+  return (
+    <div className="team">
+      <div className="member gold">Moon!</div>
+      <div className="member blue">Mercury!</div>
+      <div className="member red">Mars!</div>
+      <div className="member green">Jupiter!</div>
+      <div className="member orange">Vinus!</div>
+    </div>
+  )
+}
+
+export default SailorMoonColors;
--- code-practice/react/09-component-styling/src/App.js
+++ code-practice/react/09-component-styling/src/App.js
@@ -1,11 +1,15 @@
-import DeliveryBox from "./styled-components/what-is-styled-components/DeliveryBox";
+import DeliveryBox from "./09-04-styled-components/what-is-styled-components/DeliveryBox";
+import SailorMoonColors from "./09-02-Sass/09-02-00-Intro/SailorMoonColors";
 
 function App() {
   return (
     <div>
       <h2>DeliveryBox - by styled-components</h2>
       <DeliveryBox />
-    </div>
+      
+      <h2>SailorMoonColors - by SCSS</h2>
+      <SailorMoonColors />
+    </div> 
   );
 }
 

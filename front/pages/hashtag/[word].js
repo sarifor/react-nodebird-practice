50b69fde06e6e645c9c32a93d851b731b6553462
Sarifor <iread1q84@gmail.com>
2024-08-17 07:13:03 +0900

    Refactor: Display hashtag result within AppLayout

--- front/pages/hashtag/[word].js
+++ front/pages/hashtag/[word].js
@@ -1,9 +1,21 @@
 import React from 'react';
+import AppLayout from '../../components/AppLayout';
 import { useRouter } from 'next/router';
 
-// Path parameter로 받은 해시태그를 화면에 보여줌
+// Path parameter로 받은 해시태그를 화면에 보여주기
 // - 해당 해시태그를 사용하는 포스트 보여주기로 변경 예정
-export default function Word() {
+const Word = () => {
   const router = useRouter();
-  return <div>You selected hashtag {router.query.word}</div>
+  return (
+    <AppLayout>
+      <div>
+        You clciked hashtag {router.query.word}.<br />
+        Posts with {router.query.word} are as below.
+
+        {/* Post list */}
+      </div>
+    </AppLayout>
+  )
 }
+
+export default Word;

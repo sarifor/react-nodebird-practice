5dd5a25313765df51b48d145bd4ef92b2c4f4951
Sarifor <iread1q84@gmail.com>
2024-08-18 18:35:28 +0900

    Add: comment questioning the need for <a> as children of <Link>

--- front/components/Post/PostCardContent.js
+++ front/components/Post/PostCardContent.js
@@ -6,11 +6,12 @@ import PropTypes from 'prop-types';
 // - 포스트를 단어 단위로 쪼개놓고, #로 시작하는 단어에 링크 걺
 // - 끝에 쿼리스트링을 붙임으로, 해시태그를 클릭하여 페이지에 도달함을 나타냄
 // - CF: https://x.com/hashtag/NewJeans?src=hashtag_click
+// - Q. An <a> element is no longer required as a child of <Link> (from Next.js v13) -> 진짜 그런가 실험해보자
 const PostCardContent = ({ postData }) => {
   const dividedPostData = postData.split(/(\s+)/).map((word, index) => {
     if (word.startsWith('#')) {
       const wordWithoutHash = word.substring(1);      
-      return <Link key={index} href={`/hashtag/${wordWithoutHash}?src=hashtag_click`}>{word}</Link>
+      return <Link key={index} href={`/hashtag/${wordWithoutHash}?src=hashtag_click`}><a>{word}</a></Link>
     }
 
     return word;

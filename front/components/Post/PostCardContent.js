aee262e1e786e81f1c6d8698f05f4f5c583d6c08
Sarifor <iread1q84@gmail.com>
2024-08-20 10:43:28 +0900

    Refactor: Use regular expression to find hashtags

--- front/components/Post/PostCardContent.js
+++ front/components/Post/PostCardContent.js
@@ -3,13 +3,13 @@ import Link from "next/link";
 import PropTypes from 'prop-types';
 
 // 해시태그에 링크 걸기
-// - 포스트를 단어 단위로 쪼개놓고, #로 시작하는 단어에 링크 걺
+// - 게시글에서 공백을 기준으로 단어 단위로 쪼개놓고, #로 시작하는 단어에 링크 걺
 // - 끝에 쿼리스트링을 붙임으로, 해시태그를 클릭하여 페이지에 도달함을 나타냄
 // - CF: https://x.com/hashtag/NewJeans?src=hashtag_click
 // - Q. An <a> element is no longer required as a child of <Link> (from Next.js v13) -> 진짜 그런가 실험해보자
 const PostCardContent = ({ postData }) => {
   const dividedPostData = postData.split(/(\s+)/).map((word, index) => {
-    if (word.startsWith('#')) {
+    if (word.match(/#[\w]+/g)) {
       const wordWithoutHash = word.substring(1);      
       return <Link key={index} href={`/hashtag/${wordWithoutHash}?src=hashtag_click`}><a>{word}</a></Link>
     }

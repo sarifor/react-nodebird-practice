6fb37b8bd00398a179464b39ae476fe9f3faac97
Sarifor <iread1q84@gmail.com>
2024-08-20 11:37:09 +0900

    Refactor: Remain <a> due to using Next.js v12

--- front/components/Post/PostCardContent.js
+++ front/components/Post/PostCardContent.js
@@ -7,7 +7,7 @@ import PropTypes from 'prop-types';
 // - #로 시작하는 1개 이상 연속되는 문자열에 링크 걺
 // - 끝에 쿼리스트링을 붙임으로, 해시태그를 클릭하여 페이지에 도달함을 나타냄
 // - CF: https://x.com/hashtag/NewJeans?src=hashtag_click
-// - Q. An <a> element is no longer required as a child of <Link> (from Next.js v13) -> 진짜 그런가 실험해보자
+// - CF: An <a> element is no longer required as a child of <Link> (from Next.js v13)
 const PostCardContent = ({ postData }) => {
   const dividedPostData = postData.split(/(\s+)/).map((word, index) => {
     if (word.match(/#[\w]+/)) {

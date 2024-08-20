4f84ac55a82ab7ed52fee1617b4675377bbe0b2d
Sarifor <iread1q84@gmail.com>
2024-08-20 14:30:57 +0900

    Refactor: Use thunk pattern to execute calculation in 3 seconds

--- code-practice/react/18-async-action-management-by-redux-middlewares/src/18-03-Use-middlewares-for-async-actions/18-03-01-redux-thunk/What-is-thunk/AddNumberIn3seconds.js
+++ code-practice/react/18-async-action-management-by-redux-middlewares/src/18-03-Use-middlewares-for-async-actions/18-03-01-redux-thunk/What-is-thunk/AddNumberIn3seconds.js
@@ -5,11 +5,18 @@ const AddNumberIn3seconds = () => {
 
   const addOne = x => x + 1;
 
+  const useThunk = (x) => {
+    const thunk = () => addOne(x);
+    return thunk;
+  }
+
+  const fn = useThunk(5);
+
   useEffect(() => {
     setTimeout(() => {
-      setValue(addOne(5));
+      setValue(fn());
     }, 3000);
-  }, []);
+  }, [fn]);
 
   return (
     <div>

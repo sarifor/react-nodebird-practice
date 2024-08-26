6fb2f6ad0b665818efe56ac1e264f4bb2915cd64
Sarifor <iread1q84@gmail.com>
2024-08-26 16:44:33 +0900

    Add: Initialize redux-saga

--- front/store/configureStore.js
+++ front/store/configureStore.js
@@ -2,9 +2,13 @@ import { createWrapper } from 'next-redux-wrapper';
 import { createStore, compose, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-// redux-thunk 사용하기
+// redux-thunk
 // - 강의에선 thunkMiddleware from 'redux-thunk';
 import { thunk } from 'redux-thunk';
+
+// redux-saga
+import createSagaMiddlewar from 'redux-saga';
+import rootSaga from '../saga';
 
 // 리듀서 불러오기
 // - index.js는 디렉터리 이름만으로 불러올 수 있음
@@ -29,12 +33,15 @@ function loggerMiddleware() {
 // 스토어 설정
 // - 개발 중에만 데브툴 연결
 // - thunk 넣지 않으면 Error: Actions must be plain objects. Use custom middleware for async actions.
+// - Q. thunk는 import한 걸 바로 넣고, redux-saga는 인스턴스를 생성해 넣어주는 이유?
 const configureStore = () => {
-  const middleware = [thunk, loggerMiddleware];
+  const sagaMiddleware = createSagaMiddlewar();
+  const middleware = [thunk, sagaMiddleware, loggerMiddleware];
   const enhancer = process.env.NODE_ENV === 'production'
   ? compose(applyMiddleware(...middleware))
   : composeWithDevTools(applyMiddleware(...middleware))
   const store = createStore(reducer, enhancer);
+  store.sagaTask = sagaMiddleware.run(rootSaga);
   return store;
 };
 

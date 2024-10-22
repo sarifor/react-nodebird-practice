261e34d22ffee743ec2a28fce013efa32d5cb6bb
Sarifor <iread1q84@gmail.com>
2024-10-22 14:55:17 +0900

    Add: Modify/Delete buttons to comments
    - Display Modify/Delete buttons if the logged-in user matches the comment's author
    - Use JavaScript expressions inside {} in JSX

--- front/components/CommentList.js
+++ front/components/CommentList.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { useSelector } from 'react-redux';
+
 import { List, Comment } from 'antd';
 
+// 댓글 리스트
+// - JSX에서 자바스크립트 표현식을 사용하려면 {}로 감싸기
 const CommentList = ({ comments }) => {
-  console.log(comments);
+  const userInfo = useSelector((state) => state.user.userInfo);
+  const userId = userInfo?.id;
 
   return (
     <List
@@ -12,7 +17,20 @@ const CommentList = ({ comments }) => {
       header={`${comments.length} ${comments.length > 1 ? 'replies' : 'reply'}`}
       itemLayout="horizontal"
       renderItem={
-        (comment) => <Comment key={comment.id} author={comment.User.nickname} content={comment.content} />
+        (comment) =>
+          <Comment 
+            key={comment.id} 
+            author={comment.User.nickname} 
+            content={comment.content}
+            actions={
+              userId && comment.User.nickname === userId
+              ? [
+                  <span key="modify"><button>Modify</button></span>,
+                  <span key="delete"><button>Delete</button></span>,
+                ]
+              : []
+            } 
+          />
       }
     />
   )
@@ -21,6 +39,7 @@ const CommentList = ({ comments }) => {
 CommentList.propTypes = {
   comments: PropTypes.arrayOf(
     PropTypes.shape({
+      id: PropTypes.string.isRequired,
       User: PropTypes.object.isRequired,
       content: PropTypes.string.isRequired,
     })
